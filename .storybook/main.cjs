db81100391e8742d090a5b97b432f4a93b7e1b65
Adam Tuttle <adamtuttlecodes@gmail.com>
2022-06-29 14:23:43 -0400

    ESM compatibility fix per https://blog.logrocket.com/build-your-own-component-library-svelte/

---
+++ .storybook/main.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions'
+	],
+	framework: '@storybook/svelte'
+	// commented per advice here: https://blog.logrocket.com/build-your-own-component-library-svelte/
+	// "svelteOptions": {
+	//   "preprocess": require("../svelte.config.js").preprocess
+	// }
+};
