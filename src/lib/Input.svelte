8897db47e2c8233f2d613409bb78fef114842e75
Adam Tuttle <adamtuttlecodes@gmail.com>
2022-06-29 14:44:57 -0400

    Add an input component and stories

---
+++ src/lib/Input.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	type ValidationStatus = 'initial' | 'invalid' | 'valid';
+
+	//this is how you accept class names as a prop named "class" in svelte
+	let propClasses = '';
+	export { propClasses as class };
+
+	export let name: string;
+	export let type: string = 'text';
+	export let id: string | null = null;
+	export let value: string = '';
+	export let placeholder: string | null | undefined = null;
+	const props = { name, type, id, placeholder };
+
+	export let validateOnChange: boolean = false;
+	export let validator: CallableFunction | null | undefined = null;
+	let validationStatus: ValidationStatus = 'initial';
+	let validationMessage: string = '';
+	let validationClasses = '';
+
+	let inputClasses: string;
+	$: inputClasses =
+		'border border-gray-500 rounded px-3 py-2 ' + validationClasses + ' ' + propClasses;
+	$: validationClasses =
+		validationStatus === 'initial'
+			? ''
+			: validationStatus === 'invalid'
+			? 'border-red-500 bg-red-200'
+			: 'border-green-500 bg-white-100';
+
+	const autovalidate = () => (validateOnChange ? validate() : null);
+	export const validate = async () => {
+		if (!validator) {
+			return;
+		}
+		validationMessage = await validator(value);
+		if (validationMessage.trim() === '') {
+			validationStatus = 'valid';
+		} else {
+			validationStatus = 'invalid';
+		}
+	};
+
+	//if a value is provided at load/mount, validate it
+	if (value !== '') {
+		validate();
+	}
+</script>
+
+<input
+	class={inputClasses}
+	{...props}
+	bind:value
+	on:keyup={autovalidate}
+	on:change={autovalidate}
+/>
+{#if validationStatus === 'valid'}
+	<span class="valid" />
+{:else if validationStatus === 'invalid'}
+	<span class="validationMessage">{validationMessage}</span>
+{/if}
---
+++ src/stories/Input.stories.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Input from '../lib/Input.svelte';
+
+	/* This is how it would work, but it makes Storybook barf :( */
+	// const exampleValidator = (currentValue: string) => {
+	// 	if (currentValue.trim() !== '42') {
+	// 		return 'The only correct value is 42.';
+	// 	}
+	// 	return '';
+	// };
+</script>
+
+<Meta title="Input" component={Input} />
+
+<Template let:args>
+	<Input {...args} />
+</Template>
+
+<Story
+	name="Vanilla"
+	args={{
+		type: 'text',
+		name: 'cheese',
+		id: 'cheese',
+		value: '',
+		placeholder: 'placeholder text',
+		block: false,
+		class: ''
+	}}
+/>
+
+<Story
+	name="AutoValidating"
+	args={{
+		type: 'text',
+		value: '',
+		placeholder: 'placeholder text',
+		validateOnChange: true,
+		/* This is how it would work, but it makes Storybook barf :( */
+		// validator: exampleValidator,
+		accentuateValid: true
+	}}
+/>
