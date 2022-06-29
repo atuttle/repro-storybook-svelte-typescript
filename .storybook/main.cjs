fbf30c18c79d6ecab58bb6c5ef1753060be48e98
Adam Tuttle <adamtuttlecodes@gmail.com>
2022-06-29 14:45:17 -0400

    Fix TypeScript errors per https://stackoverflow.com/questions/65116562/how-can-typescript-be-setup-in-storybook-with-svelte/65118222#65118222

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -6,7 +6,14 @@ module.exports = {
 		'@storybook/addon-interactions',
 		'@storybook/addon-svelte-csf'
 	],
-	framework: '@storybook/svelte'
+	framework: '@storybook/svelte',
+	webpackFinal: async (config) => {
+		const svelteLoader = config.module.rules.find(
+			(r) => r.loader && r.loader.includes('svelte-loader')
+		);
+		svelteLoader.options.preprocess = require('svelte-preprocess')();
+		return config;
+	}
 	// commented per advice here: https://blog.logrocket.com/build-your-own-component-library-svelte/
 	// "svelteOptions": {
 	//   "preprocess": require("../svelte.config.js").preprocess
